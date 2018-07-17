b8efb498865dace7589642b23caecc0e67c28655
17045754 <17045754@myrp.edu.sg>
2018-07-17 12:38:53 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/simplemovielist/MainActivity.java
@@ -0,0 +1,47 @@
+package sg.edu.rp.c346.simplemovielist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Step 1b: Create a ListView Variable
+    ListView lvMovie;
+
+    //Step 2a:Create an ArrayList Variable
+
+    ArrayList<String> alMovieList;
+
+    //Step 3a: Create an ArrayAdapter
+
+    ArrayAdapter<String> aaMovie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Step 1c : Bind the UI element to the Java Variable
+        lvMovie = findViewById(R.id.listViewMovie);
+
+        //Step 2b: Initialize the ArrayList
+
+        alMovieList = new ArrayList<>();
+
+        //Step 2c: Add data into the ArrayList
+
+        alMovieList.add("Avengers Infinity War  Release Date : 2018.04");
+        alMovieList.add("Justice League  Release Date : 2017.11");
+
+        //Step 3b: Initialize the ArrayAdapter and bind it to the ArrayList ( Layout file for row item )
+        aaMovie = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,alMovieList);
+
+        //Step 4:Bind the ListView to the ArrayAdapter
+
+        lvMovie.setAdapter(aaMovie);
+    }
+}
